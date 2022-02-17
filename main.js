b46aed8e9f8321ddafe3100221721d7a786951ed
Yi BoYang <1450101587@qq.com>
2022-02-17 13:33:55 +0800

    将没有前端国际化(i18n)的英文内容改为中文

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 var googleAnalyticsService = analytics.getService('ice_cream_app');
 var googleAnalytics = googleAnalyticsService.getTracker("UA-75834706-2");
 var googleAnalyticsConfig = false;
-googleAnalyticsService.getConfig().addCallback(function (config) {
+googleAnalyticsService.getConfig().addCallback(function(config) {
     googleAnalyticsConfig = config;
 });
 
@@ -24,57 +24,57 @@ let globalSettings = {
     // Used to depict how the units are displayed within the UI
     unitType: null,
     // Used to convert units within the UI
-    osdUnits: null,    
+    osdUnits: null,
     mapProviderType: null,
     mapApiKey: null,
     proxyURL: null,
     proxyLayer: null
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
     // translate to user-selected language
     localize();
 
-    chrome.storage.local.get('unit_type', function (result) {
+    chrome.storage.local.get('unit_type', function(result) {
         if (!result.unit_type) {
             result.unit_type = UnitType.none;
         }
-        globalSettings.unitType = result.unit_type;        
+        globalSettings.unitType = result.unit_type;
     });
-    chrome.storage.local.get('map_provider_type', function (result) {
+    chrome.storage.local.get('map_provider_type', function(result) {
         if (typeof result.map_provider_type === 'undefined') {
             result.map_provider_type = 'osm';
         }
         globalSettings.mapProviderType = result.map_provider_type;
     });
-    chrome.storage.local.get('map_api_key', function (result) {
+    chrome.storage.local.get('map_api_key', function(result) {
         if (typeof result.map_api_key === 'undefined') {
             result.map_api_key = '';
         }
         globalSettings.mapApiKey = result.map_api_key;
     });
-    chrome.storage.local.get('proxyurl', function (result) {
+    chrome.storage.local.get('proxyurl', function(result) {
         if (typeof result.proxyurl === 'undefined') {
             result.proxyurl = 'http://192.168.1.222/mapproxy/service?';
         }
         globalSettings.proxyURL = result.proxyurl;
     });
-    chrome.storage.local.get('proxylayer', function (result) {
+    chrome.storage.local.get('proxylayer', function(result) {
         if (typeof result.proxylayer === 'undefined') {
             result.proxylayer = 'your_proxy_layer_name';
         }
         globalSettings.proxyLayer = result.proxylayer;
     });
-	
+
     // Resets the OSD units used by the unit coversion when the FC is disconnected.
     if (!CONFIGURATOR.connectionValid) {
         globalSettings.osdUnits = null;
     }
-    
+
     // alternative - window.navigator.appVersion.match(/Chrome\/([0-9.]*)/)[1];
-    GUI.log('Running - OS: <strong>' + GUI.operating_system + '</strong>, ' +
+    GUI.log('电脑系统: <strong>' + GUI.operating_system + '</strong>, ' +
         'Chrome: <strong>' + window.navigator.appVersion.replace(/.*Chrome\/([0-9.]*).*/, "$1") + '</strong>, ' +
-        'Configurator: <strong>' + chrome.runtime.getManifest().version + '</strong>');
+        '配置软件版本: <strong>' + chrome.runtime.getManifest().version + '</strong>');
 
     $('#status-bar .version').text(chrome.runtime.getManifest().version);
     $('#logo .version').text(chrome.runtime.getManifest().version);
@@ -101,13 +101,13 @@ $(document).ready(function () {
         var win = gui.Window.get();
 
         //Listen to the new window event
-        win.on('new-win-policy', function (frame, url, policy) {
+        win.on('new-win-policy', function(frame, url, policy) {
             gui.Shell.openExternal(url);
             policy.ignore();
         });
 
         //Get saved size and position
-        chrome.storage.local.get('windowSize', function (result) {
+        chrome.storage.local.get('windowSize', function(result) {
             if (result.windowSize) {
                 win.height = result.windowSize.height;
                 win.width = result.windowSize.width;
@@ -118,11 +118,11 @@ $(document).ready(function () {
 
         win.setMinimumSize(800, 600);
 
-        win.on('close', function () {
+        win.on('close', function() {
             //Save window size and position
-            chrome.storage.local.set({'windowSize': {height: win.height, width: win.width, x: win.x, y: win.y}}, function () {
+            chrome.storage.local.set({ 'windowSize': { height: win.height, width: win.width, x: win.x, y: win.y } }, function() {
                 // Notify that we saved.
-                console.log('Settings saved');
+                console.log('设置已保存');
             });
             this.hide(); // Pretend to be closed already
             console.log("We're closing...");
@@ -132,10 +132,10 @@ $(document).ready(function () {
         console.log('Not load require');
     }
 
-    chrome.storage.local.get('logopen', function (result) {
+    chrome.storage.local.get('logopen', function(result) {
         if (result.logopen) {
             $("#showlog").trigger('click');
-         }
+        }
     });
 
     appUpdater.checkRelease(chrome.runtime.getManifest().version);
@@ -145,9 +145,9 @@ $(document).ready(function () {
 
     // Tabs
     var ui_tabs = $('#tabs > ul');
-    $('a', ui_tabs).click(function () {
+    $('a', ui_tabs).click(function() {
 
-        if ($(this).parent().hasClass("tab_help")) {            
+        if ($(this).parent().hasClass("tab_help")) {
             return;
         }
 
@@ -177,7 +177,7 @@ $(document).ready(function () {
 
             GUI.tab_switch_in_progress = true;
 
-            GUI.tab_switch_cleanup(function () {
+            GUI.tab_switch_cleanup(function() {
                 // disable previously active tab highlight
                 $('li', ui_tabs).removeClass('active');
 
@@ -289,31 +289,31 @@ $(document).ready(function () {
     $('#tabs ul.mode-disconnected li a:first').click();
 
     // options
-    $('#options').click(function () {
+    $('#options').click(function() {
         var el = $(this);
 
         if (!el.hasClass('active')) {
             el.addClass('active');
             el.after('<div id="options-window"></div>');
 
-            $('div#options-window').load('./tabs/options.html', function () {
+            $('div#options-window').load('./tabs/options.html', function() {
                 googleAnalytics.sendAppView('Options');
 
                 // translate to user-selected language
                 localize();
 
                 // if notifications are enabled, or wasn't set, check the notifications checkbox
-                chrome.storage.local.get('update_notify', function (result) {
+                chrome.storage.local.get('update_notify', function(result) {
                     if (typeof result.update_notify === 'undefined' || result.update_notify) {
                         $('div.notifications input').prop('checked', true);
                     }
                 });
 
-                $('div.notifications input').change(function () {
+                $('div.notifications input').change(function() {
                     var check = $(this).is(':checked');
                     googleAnalytics.sendEvent('Settings', 'Notifications', check);
 
-                    chrome.storage.local.set({'update_notify': check});
+                    chrome.storage.local.set({ 'update_notify': check });
                 });
 
                 // if tracking is enabled, check the statistics checkbox
@@ -321,7 +321,7 @@ $(document).ready(function () {
                     $('div.statistics input').prop('checked', true);
                 }
 
-                $('div.statistics input').change(function () {
+                $('div.statistics input').change(function() {
                     var check = $(this).is(':checked');
                     googleAnalytics.sendEvent('Settings', 'GoogleAnalytics', check);
                     googleAnalyticsConfig.setTrackingPermitted(check);
@@ -331,11 +331,11 @@ $(document).ready(function () {
                 $('#map-provider-type').val(globalSettings.mapProviderType);
                 $('#map-api-key').val(globalSettings.mapApiKey);
                 $('#proxyurl').val(globalSettings.proxyURL);
-                $('#proxylayer').val(globalSettings.proxyLayer);        
+                $('#proxylayer').val(globalSettings.proxyLayer);
 
                 // Set the value of the unit type
                 // none, OSD, imperial, metric
-                $('#ui-unit-type').change(function () {
+                $('#ui-unit-type').change(function() {
                     chrome.storage.local.set({
                         'unit_type': $(this).val()
                     });
@@ -348,39 +348,40 @@ $(document).ready(function () {
                     }
 
                     // Horrible way to reload the tab
-                    const activeTab = $('#tabs li.active'); 
-                    activeTab.removeClass('active');  
-                    activeTab.find('a').click();            
+                    const activeTab = $('#tabs li.active');
+                    activeTab.removeClass('active');
+                    activeTab.find('a').click();
                 });
-                $('#map-provider-type').change(function () {
+                $('#map-provider-type').change(function() {
                     chrome.storage.local.set({
                         'map_provider_type': $(this).val()
                     });
                     globalSettings.mapProviderType = $(this).val();
                 });
-                $('#map-api-key').change(function () {
+                $('#map-api-key').change(function() {
                     chrome.storage.local.set({
                         'map_api_key': $(this).val()
                     });
                     globalSettings.mapApiKey = $(this).val();
                 });
-                $('#proxyurl').change(function () {
+                $('#proxyurl').change(function() {
                     chrome.storage.local.set({
                         'proxyurl': $(this).val()
                     });
                     globalSettings.proxyURL = $(this).val();
                 });
-				$('#proxylayer').change(function () {
+                $('#proxylayer').change(function() {
                     chrome.storage.local.set({
                         'proxylayer': $(this).val()
                     });
                     globalSettings.proxyLayer = $(this).val();
                 });
+
                 function close_and_cleanup(e) {
                     if (e.type == 'click' && !$.contains($('div#options-window')[0], e.target) || e.type == 'keyup' && e.keyCode == 27) {
                         $(document).unbind('click keyup', close_and_cleanup);
 
-                        $('div#options-window').slideUp(250, function () {
+                        $('div#options-window').slideUp(250, function() {
                             el.removeClass('active');
                             $(this).empty().remove();
                         });
@@ -397,7 +398,7 @@ $(document).ready(function () {
     var $content = $("#content");
 
     // listen to all input change events and adjust the value within limits if necessary
-    $content.on('focus', 'input[type="number"]', function () {
+    $content.on('focus', 'input[type="number"]', function() {
         var element = $(this),
             val = element.val();
 
@@ -406,7 +407,7 @@ $(document).ready(function () {
         }
     });
 
-    $content.on('keydown', 'input[type="number"]', function (e) {
+    $content.on('keydown', 'input[type="number"]', function(e) {
         // whitelist all that we need for numeric control
         var whitelist = [
             96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, // numpad and standard number keypad
@@ -421,7 +422,7 @@ $(document).ready(function () {
         }
     });
 
-    $content.on('change', 'input[type="number"]', function () {
+    $content.on('change', 'input[type="number"]', function() {
         var element = $(this),
             min = parseFloat(element.prop('min')),
             max = parseFloat(element.prop('max')),
@@ -472,42 +473,42 @@ $(document).ready(function () {
     });
 
     $("#showlog").on('click', function() {
-    var state = $(this).data('state'),
-        $log = $("#log");
+        var state = $(this).data('state'),
+            $log = $("#log");
 
-    if (state) {
-        $log.animate({height: 27}, 200, function() {
-             var command_log = $('div#log');
-             //noinspection JSValidateTypes
-            command_log.scrollTop($('div.wrapper', command_log).height());
-        });
-        $log.removeClass('active');
-        $("#content").removeClass('logopen');
-        $(".tab_container").removeClass('logopen');
-        $("#scrollicon").removeClass('active');
-        chrome.storage.local.set({'logopen': false});
+        if (state) {
+            $log.animate({ height: 27 }, 200, function() {
+                var command_log = $('div#log');
+                //noinspection JSValidateTypes
+                command_log.scrollTop($('div.wrapper', command_log).height());
+            });
+            $log.removeClass('active');
+            $("#content").removeClass('logopen');
+            $(".tab_container").removeClass('logopen');
+            $("#scrollicon").removeClass('active');
+            chrome.storage.local.set({ 'logopen': false });
 
-        state = false;
-    }else{
-        $log.animate({height: 111}, 200);
-        $log.addClass('active');
-        $("#content").addClass('logopen');
-        $(".tab_container").addClass('logopen');
-        $("#scrollicon").addClass('active');
-        chrome.storage.local.set({'logopen': true});
+            state = false;
+        } else {
+            $log.animate({ height: 111 }, 200);
+            $log.addClass('active');
+            $("#content").addClass('logopen');
+            $(".tab_container").addClass('logopen');
+            $("#scrollicon").addClass('active');
+            chrome.storage.local.set({ 'logopen': true });
 
-        state = true;
-    }
-    $(this).text(state ? 'Hide Log' : 'Show Log');
-    $(this).data('state', state);
+            state = true;
+        }
+        $(this).text(state ? '隐藏日志' : '显示日志');
+        $(this).data('state', state);
 
     });
 
     var profile_e = $('#profilechange');
 
-    profile_e.change(function () {
+    profile_e.change(function() {
         var profile = parseInt($(this).val());
-        MSP.send_message(MSPCodes.MSP_SELECT_SETTING, [profile], false, function () {
+        MSP.send_message(MSPCodes.MSP_SELECT_SETTING, [profile], false, function() {
             GUI.log(chrome.i18n.getMessage('pidTuningLoadedProfile', [profile + 1]));
             updateActivatedTab();
         });
@@ -515,9 +516,9 @@ $(document).ready(function () {
 
     var batteryprofile_e = $('#batteryprofilechange');
 
-    batteryprofile_e.change(function () {
+    batteryprofile_e.change(function() {
         var batteryprofile = parseInt($(this).val());
-        MSP.send_message(MSPCodes.MSP2_INAV_SELECT_BATTERY_PROFILE, [batteryprofile], false, function () {
+        MSP.send_message(MSPCodes.MSP2_INAV_SELECT_BATTERY_PROFILE, [batteryprofile], false, function() {
             GUI.log(chrome.i18n.getMessage('loadedBatteryProfile', [batteryprofile + 1]));
             updateActivatedTab();
         });
@@ -525,11 +526,11 @@ $(document).ready(function () {
 });
 
 function get_osd_settings() {
-    if (globalSettings.osdUnits !== undefined && globalSettings.osdUnits !==  null) {
+    if (globalSettings.osdUnits !== undefined && globalSettings.osdUnits !== null) {
         return;
     }
-    
-    MSP.promise(MSPCodes.MSP2_INAV_OSD_PREFERENCES).then(function (resp) {
+
+    MSP.promise(MSPCodes.MSP2_INAV_OSD_PREFERENCES).then(function(resp) {
         var prefs = resp.data;
         prefs.readU8();
         prefs.readU8();
@@ -580,10 +581,10 @@ Number.prototype.clamp = function(min, max) {
  *  "{0}:{1}:{2}".format("a","b","c") === "{0}:{1}:{2}".format("a","b").format("c")
  *  "{0}:{1}:{2}".format("a").format("b").format("c") === "{0}:{1}:{2}".format("a").format("b", "c")
  **/
-String.prototype.format = function () {
+String.prototype.format = function() {
     var args = arguments;
-    return this.replace(/\{(\d+)\}/g, function (t, i) {
-        return args[i] !== void 0 ? args[i] : "{"+(i-args.length)+"}";
+    return this.replace(/\{(\d+)\}/g, function(t, i) {
+        return args[i] !== void 0 ? args[i] : "{" + (i - args.length) + "}";
     });
 };
 
